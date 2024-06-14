7731164d9a76aebe4488cb5ba055dec27e68ebff
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-06-14 18:24:57 -0300

    feat: adicionar logica de seletores na GameActivity

--- app/src/main/java/com/isabellatressino/myapplication/GameActivity.java
+++ app/src/main/java/com/isabellatressino/myapplication/GameActivity.java
@@ -1,6 +1,8 @@
 package com.isabellatressino.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.isabellatressino.myapplication.databinding.ActivityGameBinding;
+
 public class GameActivity extends AppCompatActivity {
+
+    private ActivityGameBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_game);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityGameBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.ivScissors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectButton(binding.ivScissors);
+            }
         });
+
+        binding.ivRock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectButton(binding.ivRock);
+            }
+        });
+
+        binding.ivPaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectButton(binding.ivPaper);
+            }
+        });
+    }
+
+    private void selectButton(ImageButton selectedButton) {
+        binding.ivScissors.setSelected(false);
+        binding.ivRock.setSelected(false);
+        binding.ivPaper.setSelected(false);
+
+        selectedButton.setSelected(true);
     }
 }
