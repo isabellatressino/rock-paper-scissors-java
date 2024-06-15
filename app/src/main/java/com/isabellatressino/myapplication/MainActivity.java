3dcc42161aed6bd2c2a8ad366ebeca61a97642c3
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-06-15 12:08:07 -0300

    refactor: mudar declaração do binding na MainActivity

--- app/src/main/java/com/isabellatressino/myapplication/MainActivity.java
+++ app/src/main/java/com/isabellatressino/myapplication/MainActivity.java
@@ -9,17 +9,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.isabellatressino.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.btnPlay.setOnClickListener(new View.OnClickListener() {
