5b0fbb65f96ac516a7696dbf301f304ffbab292c
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-06-15 15:01:35 -0300

    feat: adicionar funcionalidades novas na GameActivity

--- app/src/main/java/com/isabellatressino/myapplication/GameActivity.java
+++ app/src/main/java/com/isabellatressino/myapplication/GameActivity.java
@@ -1,16 +1,22 @@
 package com.isabellatressino.myapplication;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.isabellatressino.myapplication.databinding.ActivityGameBinding;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -23,33 +29,82 @@ public class GameActivity extends AppCompatActivity {
         binding = ActivityGameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.ivScissors.setOnClickListener(new View.OnClickListener() {
+        binding.ibScissors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectButton(binding.ivScissors);
+                selectButton(binding.ibScissors, "scissors");
             }
         });
 
-        binding.ivRock.setOnClickListener(new View.OnClickListener() {
+        binding.ibRock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectButton(binding.ivRock);
+                selectButton(binding.ibRock, "rock");
             }
         });
 
-        binding.ivPaper.setOnClickListener(new View.OnClickListener() {
+        binding.ibPaper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectButton(binding.ivPaper);
+                selectButton(binding.ibPaper, "paper");
             }
         });
     }
 
-    private void selectButton(ImageButton selectedButton) {
-        binding.ivScissors.setSelected(false);
-        binding.ivRock.setSelected(false);
-        binding.ivPaper.setSelected(false);
+    private void selectButton(ImageButton selectedButton, String option) {
+        binding.ibScissors.setSelected(false);
+        binding.ibScissors.setEnabled(false);
+
+        binding.ibRock.setSelected(false);
+        binding.ibRock.setEnabled(false);
+
+        binding.ibPaper.setSelected(false);
+        binding.ibPaper.setEnabled(false);
 
         selectedButton.setSelected(true);
+
+        checkVictory(option);
     }
+
+    private String ramdomChoice() {
+        String[] options = {"rock", "scissors", "paper"};
+        int ramdomNumber = new Random().nextInt(3);
+        switch (ramdomNumber) {
+            case 0:
+                binding.ibGuess.setImageResource(R.drawable.rock);
+                break;
+            case 1:
+                binding.ibGuess.setImageResource(R.drawable.scissors);
+                break;
+            case 2:
+                binding.ibGuess.setImageResource(R.drawable.paper);
+                break;
+        }
+
+        return options[ramdomNumber];
+    }
+
+    private void checkVictory(String userChoice) {
+        String appChoice = ramdomChoice();
+
+        String txt = "";
+
+        if ((userChoice.equals("rock") && appChoice.equals("scissors")) ||
+                (userChoice.equals("scissors") && appChoice.equals("paper")) ||
+                (userChoice.equals("paper") && appChoice.equals("rock"))) {
+            txt = "Você Ganhou";
+        } else if ((userChoice.equals("rock") && appChoice.equals("paper")) ||
+                (userChoice.equals("scissors") && appChoice.equals("rock")) ||
+                (userChoice.equals("paper") && appChoice.equals("scissors"))) {
+            txt = "Você Perdeu";
+        } else {
+            txt = "Empate";
+        }
+
+        binding.tvGameResult.setText(txt);
+        binding.ibReplay.setVisibility(View.VISIBLE);
+
+    }
+
+
 }
